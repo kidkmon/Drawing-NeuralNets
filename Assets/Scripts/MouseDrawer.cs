644d7ffdea896a41bfaff124cfad5fcb4751015a
kidkmon <kid.mon@hotmail.com>
2018-07-11 17:07:28 -0400

    first commit

---
+++ Assets/Scripts/MouseDrawer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MouseDrawer : MonoBehaviour {
+
+	private new Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
