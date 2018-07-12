5fcceecf1231b6b46b62608f4fadebadf405acaa
kidkmon <kid.mon@hotmail.com>
2018-07-12 18:12:39 -0400

    adding the update scenario and instatiating objects with the response

--- Assets/Scripts/MouseDrawer.cs
+++ Assets/Scripts/MouseDrawer.cs
@@ -1,83 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using SocketIO;
-using System.IO;
 using UnityEngine.UI;
+using SocketIO;
 using System;
-// using UnityEngine.text;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class MouseDrawer : MonoBehaviour {
 
 	public Camera camera;
-	
-	public Text text;
-
+	public Text resultText;
 	public float speed = 5;
+	public GameObject[] baseballPrefabs;
+
 	private Vector3 mousePosition;
 	private SocketIOComponent socket;
-	// public Text instruction;
-
+	private JSONObject dataResult;
+	private bool ballInstiate, batInstatiate;
 
 	void Start(){
 		GameObject go = GameObject.Find("SocketIO");
 		socket = go.GetComponent<SocketIOComponent>();
 
 		socket.On("open", TestOpen);
-		
-		// socket.On("open", TestOpen);
-		// socket.On("boop", TestBoop);
-		// socket.On("reply", TestReply);
 		socket.On("reply", OnReply);
 		socket.On("error", TestError);
 		socket.On("close", TestClose);
 
-
-	
-		// instruction = GetComponent<Text>();
-		text.text = "begin";
+		resultText.text = "Start";
 		StartCoroutine("BeepBoop");
-
-		// instruction.GetComponent<UnityEngine.UI.Text>().text = "testing";
-	}
-
-	private IEnumerator BeepBoop()
-	{
-		// wait 1 seconds and continue
-		yield return new WaitForSeconds(1);
-		
-		socket.Emit("beep");
-		
-		// wait 3 seconds and continue
-		yield return new WaitForSeconds(3);
-		
-		socket.Emit("beep");
-		
-		// wait 2 seconds and continue
-		yield return new WaitForSeconds(2);
-		
-		socket.Emit("beep");
-		
-		// wait ONE FRAME and continue
-		yield return null;
-		
-		socket.Emit("beep");
-		socket.Emit("beep");
 	}
 
 	// Update is called once per frame
 	void Update () {
+		
 		if(Input.GetMouseButton(0)){
 			Vector3 temp = Input.mousePosition;
 			temp.z = 10;
 			transform.position = Camera.main.ScreenToWorldPoint(temp);
 			GetComponent<TrailRenderer>().enabled = true;
 		}
+		
 		if(Input.GetMouseButtonUp(0)){
 			StartCoroutine("WriteAndSendPng");
 		}
-		// socket.On("reply", TestReply);
+
+		if(dataResult != null){
+			if(dataResult.ToString() == "\"ball\"" && !ballInstiate){
+				ballInstiate = true;
+				Instantiate(baseballPrefabs[0],baseballPrefabs[0].transform.position, baseballPrefabs[0].transform.rotation);
+			}	
+
+			if(dataResult.ToString() == "\"baseball bat\"" && !batInstatiate){
+				batInstatiate = true;
+				Instantiate(baseballPrefabs[1],baseballPrefabs[1].transform.position, baseballPrefabs[1].transform.rotation);
+			}
+		}
+	
 	}
 
 
@@ -101,49 +80,60 @@ public class MouseDrawer : MonoBehaviour {
 		socket.Emit("chat message", j);
 		Destroy(tex);
 		System.IO.File.WriteAllBytes("test.png", bytes);
-
-		// socket.On("reply", TestReply);
-
 		yield return new WaitForSeconds(1f);
 	}
 
 
-	public void TestOpen(SocketIOEvent e)
-	{
-		Debug.Log("[SocketIO] Open received: " + e.name + " " + e.data);
+	private IEnumerator BeepBoop(){
+		// wait 1 seconds and continue
+		yield return new WaitForSeconds(1);
+		
+		socket.Emit("beep");
+		
+		// wait 3 seconds and continue
+		yield return new WaitForSeconds(3);
+		
+		socket.Emit("beep");
+		
+		// wait 2 seconds and continue
+		yield return new WaitForSeconds(2);
+		
+		socket.Emit("beep");
+		
+		// wait ONE FRAME and continue
+		yield return null;
+		
+		socket.Emit("beep");
+		socket.Emit("beep");
+	}
+
+	public void TestOpen(SocketIOEvent e){
+		//Debug.Log("[SocketIO] Open received: " + e.name + " " + e.data);
 	}
 	
-	public void TestBoop(SocketIOEvent e)
-	{
-		Debug.Log("[SocketIO] Boop received: " + e.name + " " + e.data);
+	public void TestBoop(SocketIOEvent e){
+		//Debug.Log("[SocketIO] Boop received: " + e.name + " " + e.data);
 
 		if (e.data == null) { return; }
 
-		Debug.Log(
-			"#####################################################" +
-			"THIS: " + e.data.GetField("this").str +
-			"#####################################################"
-		);
+		// Debug.Log(
+		// 	"#####################################################" +
+		// 	"THIS: " + e.data.GetField("this").str +
+		// 	"#####################################################"
+		// );
 	}
 	
-	public void TestError(SocketIOEvent e)
-	{
-		Debug.Log("[XXXXXXXX] Error received: " + e.name + " " + e.data);
+	public void TestError(SocketIOEvent e){
+		//Debug.Log("[XXXXXXXX] Error received: " + e.name + " " + e.data);
 	}
 	
-	public void TestClose(SocketIOEvent e)
-	{	
-		Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
+	public void TestClose(SocketIOEvent e){	
+		//Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
 	}
 
-	public void OnReply(SocketIOEvent e)
-	{
-		//Debug.Log("[SocketIO] OnCharacter received: " + e.name + " " + e.data);
-		text.text = e.data.ToString();
-
-		print(e.data.ToString());
-
-		// GameObject go = GameObject.Find (ch.objectName);
-		// go.transform.position = new Vector3 (ch.x, 0, -ch.y);
+	public void OnReply(SocketIOEvent e){
+		dataResult = e.data[1];
+		resultText.text = e.data[1].ToString();
+		print(e.data[1].ToString());
 	}
 }
